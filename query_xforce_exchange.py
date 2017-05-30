760cbeb8698ea8f595de05eb18b131b0bc3389d4
Miclain K Keffeler <mkkeffeler@crimson.ua.edu>
2017-05-30 12:23:56 -0500

    Update query_xforce_exchange.py
    Updated report output and comments

--- query_xforce_exchange.py
+++ query_xforce_exchange.py
@@ -4,13 +4,16 @@ import json
 from optparse import OptionParser
 import hashlib
 import base64
+#Output all downloaded json to a file
 output = open("output.json","w")
+
 def send_request(apiurl, scanurl, headers,output):
     fullurl = apiurl +  scanurl
     response = requests.get(fullurl, params='', headers=headers, timeout=20)
     all_json = response.json()
     output.write(json.dumps(all_json,indent=4,sort_keys=True))
     return all_json
+
 def get_md5(filename):
     try:
         f = open(filename,"rb")
@@ -18,8 +21,9 @@ def get_md5(filename):
         return md5
     except e:
         print str(e)
+
 if __name__ == "__main__":
-    output = open("output.json","w")
+#X-Force API Key and Password associated with your IBMID
     key = "<API_KEY>"
     password ="<API_PASSWORD>"
 
@@ -29,18 +33,24 @@ if __name__ == "__main__":
 
 
     parser = OptionParser()
+    #Use this option to check a URL
     parser.add_option("-u", "--url", dest="s_url", default=None, 
                       help="URL to be checked by Exchange IBM Xforce", metavar="scanurl")
+    #Use this option to get malware associated with an entered URL
     parser.add_option("-l", "--malwareurl", dest="m_url", default=None, 
                       help="Returns the malware associated with the entered URL", metavar="scanurl")
+    #Use this option to check a file's maliciousness
     parser.add_option("-f", "--file", dest="malfile" , default=None,
                       help="file (md5 hash) to be checked by Exchange IBM Xforce", metavar="filename")
+    #Use this option to check a md5 hash in general
     parser.add_option("-m", "--md5", dest="hash" , default=None,
                       help="hash to be checked by Exchange IBM Xforce", metavar="hashvalue")
+    #Use this option to specify an XFID
     parser.add_option("-x", "--xfid", dest="s_xfid" , default=None,
                       help="XFID to be used ", metavar="xfid")
     parser.add_option("-c", "--cve", dest="s_cve" , default=None,
                       help="CVE, BID, US-Cert, UV#, RHSA id to be searched ", metavar="cve-xxx-xxx")
+    #Use this option to check an IP address
     parser.add_option("-i", "--ip", dest="s_ip" , default=None,
                       help="ip to be checked", metavar="ipaddress")
 (options, args) = parser.parse_args()
@@ -62,9 +72,9 @@ elif (options.s_ip is not None):
     apiurl = url + "/ipr/"
     all_json = send_request(apiurl, scanurl, headers,output)
     apiurl = url + "/ipr/malware/"
-#    all_json = send_request(apiurl, scanurl, headers,output)
+    send_request(apiurl, scanurl, headers,output)
     apiurl = url + "/ipr/history/"
- #   all_json = send_request(apiurl, scanurl, headers,output)
+    alsend_request(apiurl, scanurl, headers,output)
 elif (options.malfile is not None ):
     md5 = get_md5(options.malfile)
     if md5:
@@ -73,24 +83,28 @@ elif (options.s_xfid is not None ):
     send_request(url+"/vulnerabilities/", options.s_xfid, headers,output)
 elif (options.hash is not None ):
     send_request(url+"/ipr/", options.hash, headers,output)
-
+    
+#Print the location associated with this IP/URL
 print all_json["geo"]["country"]
-already_printed=[]
+#Used to hold categories of an IP or URL that have already been listed in the report.
+already_categorized=[]
 result_str = ""
-parts = [0,1,2,3,4,5,6,7,8,9]
+#Print the creation date of this IP/URL
 print all_json['history'][0]['created']
+#For all the entries in the history
 for key in all_json['history']:
-    for entry in key:
-        if(entry in already_printed):
-           continue
-    else:
-        for key in all_json['history']:
-           for entry in key["categoryDescriptions"]:
-               if(entry in already_printed):
-                   continue
-               else:
-                   result_str = result_str + str(entry)+" "+str(key["created"])+"\n" 
-                   already_printed.append(entry)
+    #For every entry in the json output 
+    for key in all_json['history']:
+    #For every categorization within that entrys "categoryDescriptions"
+        for entry in key["categoryDescriptions"]:
+    #If this categorization has already been reported, don't report it again
+            if(entry in already_categorized):
+                continue
+            else:
+    #Print the categorization listed and when it was classified as such
+                result_str = result_str + str(entry)+" "+str(key["created"])+"\n" 
+    #Add the category to the list of already categorized
+                already_categorized.append(entry)
 print result_str
 if len(sys.argv[1:]) == 0:
     parser.print_help()
