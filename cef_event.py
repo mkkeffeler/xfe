e0208ad4a56e54c98c6f1d5170a63df318522996
Miclain Keffeler <mkkeffeler@crimson.ua.edu>
2017-06-07 14:44:19 -0500

    CEF Event Creation being worked on.

--- cef_event.py
+++ cef_event.py
@@ -3,6 +3,9 @@
 #This script holds the function needed to generate a CEF (Common Event Format) Event. 
 #This can be called when a change is detected between historic and current data.
 #A CEF event will then be generated and this can be fed to SIEM software such as HP Arcsight.
+import datetime
 
 def generate_cef_event(old_location,old_date,old_score,old_category,new_location,new_date,new_score,new_category):
-    print "We will now generate a CEF formatted event"
+    cef = 'CEF:0|X-Force|X-Force|1.0|1.0|4|end='+str(datetime.datetime.now().strftime('%Y-%m-%DT%H:%M:%S:%fZ'))+' flexnumber1Label=Old_Score flexnumber1='+str(old_score)+' flexnumber2Label=New_Score flexnumber2='+str(new_score)    
+    print cef
+
